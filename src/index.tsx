5c78a8b4f73aa6a58043382c7e581bc4d9187e78
youngdong82 <nfs82young@gmail.com>
2025-06-17 21:33:52 +0900

    feat: index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './shared/GlobalStyle';
+import AppNew from './AppNew';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <GlobalStyle />
-    <App />
+    <AppNew />
   </>
 );
